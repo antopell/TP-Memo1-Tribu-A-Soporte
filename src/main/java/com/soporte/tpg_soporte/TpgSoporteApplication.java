617bf78b156abd58627ab996ce234d309e2a4b42
antopell <62953936+antopell@users.noreply.github.com>
2023-06-08 19:25:57 -0300

    Estructura base

---
+++ src/main/java/com/soporte/tpg_soporte/TpgSoporteApplication.java
@@ -0,0 +1,13 @@
+package com.soporte.tpg_soporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpgSoporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpgSoporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soporte/tpg_soporte/TpgSoporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soporte.tpg_soporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpgSoporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
